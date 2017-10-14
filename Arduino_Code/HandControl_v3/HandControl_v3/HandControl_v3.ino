e3313a72fedabac1a7f577823734dd42afc32b37
RishabChawla <rishychawla@gmail.com>
2017-10-14 19:05:39 -0400

    Changed the gesture waveout back to doubletap for testing

--- Arduino_Code/HandControl_v3/HandControl_v3/HandControl_v3.ino
+++ Arduino_Code/HandControl_v3/HandControl_v3/HandControl_v3.ino
@@ -2,7 +2,7 @@
 #include <Servo.h>
 #define FIST_PIN 10 //we throw paper hand
 #define FINGERSSPREAD_PIN 11 //we throw scissors
-#define WAVEOUT_PIN 9 //we throw rock
+#define DOUBLETAP_PIN 9 //we throw rock
 Servo rock;
 
 Servo paper;
@@ -16,11 +16,11 @@ void setup() {
 
   //pinMode(FIST_PIN, OUTPUT);
   //pinMode(WAVEIN_PIN, OUTPUT);
-  //pinMode(WAVEOUT_PIN, OUTPUT);
+  //pinMode(DOUBLETAP_PIN, OUTPUT);
   //pinMode(FINGERSSPREAD_PIN, OUTPUT);
-  //pinMode(WAVEOUT_PIN, OUTPUT);
+  //pinMode(DOUBLETAP_PIN, OUTPUT);
   paper.attach(FIST_PIN);  // attaches the servo on pin 9 to the servo object
-  rock.attach(WAVEOUT_PIN);  // attaches the servo on pin 9 to the servo object
+  rock.attach(DOUBLETAP_PIN);  // attaches the servo on pin 9 to the servo object
   scissors.attach(FINGERSSPREAD_PIN);  // attaches the servo on pin 9 to the servo object
 
   
@@ -40,14 +40,10 @@ void loop()
     case fingersSpread:
       throwScissors();
       break;
-    case waveOut:
+    case doubleTap:
       throwRock();
       break;
-     default:
-      setNeutral();
-      break;
    } 
-   delay(10);
     //demo();
 }
 
@@ -63,20 +59,20 @@ void throwPaper()
 {
   paper.write(UP_POS);
   delay(KEEP_UP_TIME);
-  //setNeutral();
+  setNeutral();
   
 }
 void throwScissors()
 {
   scissors.write(UP_POS);
   delay(KEEP_UP_TIME);
-  //setNeutral();  
+  setNeutral();  
 }
 void throwRock()
 {
   rock.write(UP_POS);
   delay(KEEP_UP_TIME);
-  //setNeutral();
+  setNeutral();
 
 }
 
